300ab2258d0683b0020fba3c73ecba788890640e
sreegayathri <sreegayathri13012000@gmail.com>
2025-12-29 21:35:31 +0530

    project setup and basic signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+ import { Header } from './header/header';
+import { User } from './user/user';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ Header, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('AngularApp');
 }
